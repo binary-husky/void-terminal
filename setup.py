b385d9e86293f2cde2534901196ceb47579156fa
binary-husky <qingxu.fu@outlook.com>
2023-10-23 22:30:03 +0800

    version up

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ extra_files = package_files('void_terminal',
 
 setuptools.setup(
     name="void-terminal",
-    version="0.0.7",
+    version="0.0.8",
     author="Qingxu",
     author_email="505030475@qq.com",
     description="LLM based APIs",
